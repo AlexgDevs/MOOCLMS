f03f37a024736c3ca5e33e996268105811e96c2c
AlexgDevs <asmetanin014@gmail.com>
2025-11-17 23:23:05 +0500

    feat: fix client

--- run_client.py
+++ run_client.py
@@ -1,4 +1,6 @@
+from os import getenv
 from client import app
+from dotenv import load_dotenv
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=int(getenv('PORT')), host='0.0.0.0', debug=False)
